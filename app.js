f722a9630007adbb58d3a80d4bbdc2b01b7e0ec0
Victor Hugo <vhs@certi.org.br>
2021-08-09 21:14:34 -0300

    commit1

---
+++ app.js
@@ -0,0 +1,15 @@
+var calculadora = require("./calculadora");
+
+// var mostrarSite = true;
+// const site = "www.site.com"
+
+// console.log("hello World!");
+
+// if(mostrarSite){
+//     console.log(site)
+// }
+
+
+console.log(calculadora.soma(10,20))
+console.log(calculadora.nome)
+calculadora.nome = "nome"
