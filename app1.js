89b82249b435d66993fe03b0f77253976bf76d09
Victor Hugo <vhs@certi.org.br>
2021-08-25 23:05:21 -0300

    First

---
+++ app1.js
@@ -0,0 +1,6 @@
+var http = require("http");
+
+http.createServer(function(req, res){
+    res.end("<h1>Bem vindo ao meu site !</h1>");
+}).listen(8181);
+console.log("Meu servidor esta rodando ...")
