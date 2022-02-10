54fdb27e957a4e6a816b1097a7330ae02148d425
vhs <vhs@certi.org.br>
2022-02-09 23:45:09 -0300

    aula 30

--- GuiaPerguntas/index.js
+++ GuiaPerguntas/index.js
@@ -4,25 +4,13 @@ const app = express();
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
-app.get('/:nome/:lang', (req, res) => {
-    //res.send("Bem vindo ao meu site!");
-    var nome = req.params.nome;
-    var lang = req.params.lang;
-    var exibirMsg = true;
-
-    var produtos = [
-       "Doritos", "Coca-cola", "Leite"
-    ]
-
-    res.render("index",{
-        nome: nome,
-        lang: lang,
-        empresa: "udemy",
-        inscritos: 8000,
-        msg: exibirMsg,
-        produtos: produtos
-    })
+app.get('/', (req, res) => {
+    res.render("index")
 });
+
+app.get("/perguntar",(req, res) => {
+    res.render("perguntar")
+})
 
 app.listen(8080, () => {
     console.log("App rodando !");
